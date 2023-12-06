e6f9ef5ca798dc15a1b4108909b259822c0c523d
k3yJ3y <76413543+k3yJ3y@users.noreply.github.com>
2023-12-06 22:54:18 +0100

    Added validation on the frontend

--- frontend/src/components/CreateForm.tsx
+++ frontend/src/components/CreateForm.tsx
@@ -8,21 +8,67 @@ const CreateForm: React.FC<CreateFormProps> = ({ addReservation }) => {
   const [title, setTitle] = useState<string>('');
   const [startAt, setStartAt] = useState<string>('');
   const [endAt, setEndAt] = useState<string>('');
+  const [titleError, setTitleError] = useState<string | null>(null);
+  const [startAtError, setStartAtError] = useState<string | null>(null);
+  const [endAtError, setEndAtError] = useState<string | null>(null);
+
+  const validateForm = () => {
+    let isValid = true;
+
+    // Check if all fields are filled
+    if (!title) {
+      setTitleError('Title is required');
+      isValid = false;
+    } else {
+      setTitleError(null);
+    }
+
+    if (!startAt) {
+      setStartAtError('Start date and time are required');
+      isValid = false;
+    } else {
+      setStartAtError(null);
+    }
+
+    if (!endAt) {
+      setEndAtError('End date and time are required');
+      isValid = false;
+    } else {
+      setEndAtError(null);
+    }
+
+    // Check if startAt is in the future
+    const now = new Date().toISOString().split('.')[0]; // Get current time in ISO format without milliseconds
+    if (startAt < now) {
+      setStartAtError('Start date and time must be in the future');
+      isValid = false;
+    }
+
+    // Check if endAt is later than startAt
+    if (endAt <= startAt) {
+      setEndAtError('End date and time must be later than Start date and time');
+      isValid = false;
+    }
+
+    return isValid;
+  };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const newReservation = {
-      title,
-      start_at: startAt,
-      end_at: endAt,
-    };
+    if (validateForm()) {
+      const newReservation = {
+        title,
+        start_at: startAt,
+        end_at: endAt,
+      };
 
-    addReservation(newReservation);
+      addReservation(newReservation);
 
-    setTitle('');
-    setStartAt('');
-    setEndAt('');
+      setTitle('');
+      setStartAt('');
+      setEndAt('');
+    }
   };
 
   return (
@@ -37,6 +83,8 @@ const CreateForm: React.FC<CreateFormProps> = ({ addReservation }) => {
           margin='normal'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          error={!!titleError}
+          helperText={titleError}
           required
         />
 
@@ -44,26 +92,36 @@ const CreateForm: React.FC<CreateFormProps> = ({ addReservation }) => {
           <Grid item xs={6}>
             <TextField
               inputProps={{ 'data-testid': 'start_at-input' }}
-              helperText='Please enter start date and time'
+              label='Start At'
               type='datetime-local'
               variant='outlined'
               fullWidth
               margin='normal'
               value={startAt}
               onChange={(e) => setStartAt(e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              error={!!startAtError}
+              helperText={startAtError}
               required
             />
           </Grid>
           <Grid item xs={6}>
             <TextField
               inputProps={{ 'data-testid': 'end_at-input' }}
-              helperText='Please enter end date and time'
+              label='End At'
               type='datetime-local'
               variant='outlined'
               fullWidth
               margin='normal'
               value={endAt}
               onChange={(e) => setEndAt(e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              error={!!endAtError}
+              helperText={endAtError}
               required
             />
           </Grid>
@@ -73,7 +131,7 @@ const CreateForm: React.FC<CreateFormProps> = ({ addReservation }) => {
           type='submit'
           variant='contained'
           color='primary'
-          sx={{ marginTop: '10px' }}
+          sx={{ marginTop: '15px' }}
           fullWidth
         >
           Add Reservation
